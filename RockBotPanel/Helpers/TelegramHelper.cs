9647c07a4464a5402fa77cab5b023a743f955922
Roker2 <dimaminko2000@gmail.com>
2020-10-24 22:01:01 +0300

    Helpers: TelegramHelper: Init

---
+++ RockBotPanel/Helpers/TelegramHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockBotPanel.Helpers
+{
+    public class TelegramHelper
+    {
+        public static void SendCode(int UserID, int Code)
+        {
+            Telegram.Bot.TelegramBotClient Bot = new Telegram.Bot.TelegramBotClient("TOKEN");
+            Bot.SendTextMessageAsync(UserID, Code.ToString());
+        }
+    }
+}
