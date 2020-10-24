14d53f81b3c904a4176187227827b13bc5f31648
Roker2 <dimaminko2000@gmail.com>
2020-10-24 17:34:38 +0300

    Views: Shared: Move header links to Helpers.LayoutHelper

---
+++ RockBotPanel/Helpers/LayoutHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockBotPanel.Helpers
+{
+    public class LayoutHelper
+    {
+        public static Dictionary<string, string> GetHeaderPages()
+        {
+            return new Dictionary<string, string>
+            {
+                {"Index", "Home"},
+                {"Privacy", "Privacy"}
+            };
+        }
+    }
+}
