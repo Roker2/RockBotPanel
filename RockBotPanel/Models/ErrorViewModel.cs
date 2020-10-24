ff903905f6402b737a2d7429d6160d94ef949ffc
Roker2 <dimaminko2000@gmail.com>
2020-10-24 17:04:11 +0300

    Add project files.

---
+++ RockBotPanel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RockBotPanel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
