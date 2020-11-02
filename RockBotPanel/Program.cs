9186c5950caec0d351ec3c1447a5d2bf15b10a38
Roker2 <dimaminko2000@gmail.com>
2020-11-02 15:06:29 +0300

    Revert "Add Azure support"
    This reverts commit 2a624488973d64141ebcdd03bff78192abf31c0a.

--- RockBotPanel/Program.cs
+++ RockBotPanel/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Azure.Identity;
 
 namespace RockBotPanel
 {
@@ -19,13 +18,6 @@ namespace RockBotPanel
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-.ConfigureAppConfiguration((context, config) =>
-{
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultRoker2"));
-config.AddAzureKeyVault(
-keyVaultEndpoint,
-new DefaultAzureCredential());
-})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
