bf0a89a6f86cb87ac573944f92477d7740ba9f17
Roker2 <dimaminko2000@gmail.com>
2020-11-04 16:26:20 +0300

    Hello database

---
+++ RockBotPanel/Data/PanelDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RockBotPanel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockBotPanel.Data
+{
+    public class PanelDbContext : DbContext
+    {
+        public PanelDbContext (DbContextOptions<PanelDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<TelegramUser> TelegramUser { get; set; }
+    }
+}
--- RockBotPanel/Startup.cs
+++ RockBotPanel/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RockBotPanel.Data;
 
 namespace RockBotPanel
 {
@@ -24,6 +26,9 @@ namespace RockBotPanel
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<PanelDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("PanelDbContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
