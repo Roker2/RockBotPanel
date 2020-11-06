a86f0f2f89f2140a5ad1b5cf47886e79647242ad
Roker2 <dimaminko2000@gmail.com>
2020-11-06 16:47:29 +0300

    Models: TelegramUser: Init

---
+++ RockBotPanel/Models/TelegramUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockBotPanel.Models
+{
+    public class TelegramUser
+    {
+        [Key]
+        public uint Id { get; set; }
+        public int TelegramId { get; set; }
+        public IdentityUser User { get; set; }
+    }
+}
