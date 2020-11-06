39a1305b85448bec10114e2c03636a7c692eda3e
Roker2 <dimaminko2000@gmail.com>
2020-11-06 18:54:09 +0300

    Add ChatInfo model and controller

---
+++ RockBotPanel/Models/ChatInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockBotPanel.Models
+{
+    public class ChatInfo
+    {
+        public int Id { get; set; }
+        public int warns_quantity { get; set; }
+        public string welcome { get; set; }
+        public string rules { get; set; }
+        public string disabled_commands { get; set; }
+    }
+}
