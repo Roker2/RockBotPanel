60170a732ea79cf310dfd2829ac23485f6507cde
Roker2 <dimaminko2000@gmail.com>
2020-11-12 19:18:25 +0300

    Controllers: AccountController: Add Log In

--- RockBotPanel/Controllers/AccountController.cs
+++ RockBotPanel/Controllers/AccountController.cs
@@ -64,5 +64,30 @@ namespace RockBotPanel.Controllers
             await signInManager.SignOutAsync();
             return RedirectToAction("index", "home");
         }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(
+                    model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+            }
+
+            return View(model);
+        }
     }
 }
