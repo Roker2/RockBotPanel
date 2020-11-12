6cbfe9a5accb13c56798ec352d583926422be0b2
Roker2 <dimaminko2000@gmail.com>
2020-11-12 22:57:08 +0300

    Models: TelegramUser: Add ChatIds

--- RockBotPanel/Models/TelegramUser.cs
+++ RockBotPanel/Models/TelegramUser.cs
@@ -10,5 +10,7 @@ namespace RockBotPanel.Models
     public class TelegramUser : IdentityUser
     {
         public int TelegramId { get; set; }
+
+        public String ChatIds { get; set; } // id1|id2|id3
     }
 }
