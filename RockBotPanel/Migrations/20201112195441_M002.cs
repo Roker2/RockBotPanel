99ee94e2391725fc92a7f138c9ea7cc1bfb61db2
Roker2 <dimaminko2000@gmail.com>
2020-11-12 22:57:25 +0300

    Migrations: M002

---
+++ RockBotPanel/Migrations/20201112195441_M002.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RockBotPanel.Migrations
+{
+    public partial class M002 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ChatIds",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChatIds",
+                table: "AspNetUsers");
+        }
+    }
+}
