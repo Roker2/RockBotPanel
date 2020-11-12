de141b63fd67fdc7b71dda529e17e6c1b6570317
Roker2 <dimaminko2000@gmail.com>
2020-11-12 19:07:57 +0300

    Controllers: AccountController: Init

--- RockBotPanel/Controllers/AccountController.cs
+++ RockBotPanel/Controllers/AccountController.cs
@@ -1,13 +1,61 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using RockBotPanel.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RockBotPanel.Models;
+using System.Threading.Tasks;
 
 namespace RockBotPanel.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<TelegramUser> userManager;
+        private readonly SignInManager<TelegramUser> signInManager;
+
+        public AccountController(UserManager<TelegramUser> userManager,
+            SignInManager<TelegramUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Copy data from RegisterViewModel to IdentityUser
+                var user = new TelegramUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+
+                // Store user data in AspNetUsers database table
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                // If user is successfully created, sign-in the user using
+                // SignInManager and redirect to index action of HomeController
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("index", "home");
+                }
+
+                // If there are any errors, add them to the ModelState object
+                // which will be displayed by the validation summary tag helper
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View(model);
         }
     }
 }
