457d32e3362fa09cd96252902661a1c67e755902
Roker2 <dimaminko2000@gmail.com>
2020-11-12 19:07:26 +0300

    Migrations: M001

--- RockBotPanel/Migrations/20201112155555_M001.Designer.cs
+++ RockBotPanel/Migrations/20201112155555_M001.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RockBotPanel.Data;
 
 namespace RockBotPanel.Migrations
 {
     [DbContext(typeof(PanelDbContext))]
-    partial class PanelDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201112155555_M001")]
+    partial class M001
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RockBotPanel/Migrations/20201112155555_M001.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RockBotPanel.Migrations
+{
+    public partial class M001 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TelegramId",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "TelegramId",
+                table: "AspNetUsers");
+        }
+    }
+}
