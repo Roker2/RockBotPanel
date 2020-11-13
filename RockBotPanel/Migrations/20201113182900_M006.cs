8f00ba6e1a2f16bfeaf0322c742b1321ba5e147d
Roker2 <dimaminko2000@gmail.com>
2020-11-13 21:31:24 +0300

    Migrations: M006 and M007
    Experiments

---
+++ RockBotPanel/Migrations/20201113182900_M006.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RockBotPanel.Migrations
+{
+    public partial class M006 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastValidationCode",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LastValidationCode",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
---
+++ RockBotPanel/Migrations/20201113183048_M007.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RockBotPanel.Migrations
+{
+    public partial class M007 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LastValidationCode",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastValidationCode",
+                table: "AspNetUsers");
+        }
+    }
+}
