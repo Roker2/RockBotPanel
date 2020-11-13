5f37a226a0588c6f95822edfd262399b5edaaf08
Roker2 <dimaminko2000@gmail.com>
2020-11-13 21:07:49 +0300

    Migrations: M005

--- RockBotPanel/Migrations/20201113180311_M005.Designer.cs
+++ RockBotPanel/Migrations/20201113180311_M005.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RockBotPanel.Data;
 
 namespace RockBotPanel.Migrations
 {
     [DbContext(typeof(PanelDbContext))]
-    partial class PanelDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201113180311_M005")]
+    partial class M005
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
