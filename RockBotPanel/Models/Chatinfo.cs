2e2c014825515ca46aecc43500580760d352f6e5
Roker2 <dimaminko2000@gmail.com>
2020-11-13 19:53:51 +0300

    Models: Chatinfo: If values is not avaible, use default values

--- RockBotPanel/Models/Chatinfo.cs
+++ RockBotPanel/Models/Chatinfo.cs
@@ -1,14 +1,45 @@
-﻿using System;
+﻿using RockBotPanel.Helpers;
+using System;
 using System.Collections.Generic;
 
 namespace RockBotPanel.Models
 {
     public partial class Chatinfo
     {
+        private int? _WarnsQuantity;
+        private string _Welcome;
+        private string _Rules;
         public long Id { get; set; }
-        public int? WarnsQuantity { get; set; }
-        public string Welcome { get; set; }
-        public string Rules { get; set; }
+        public int? WarnsQuantity
+        {
+            get
+            {
+                if (!_WarnsQuantity.HasValue)
+                    return 5;
+                return _WarnsQuantity;
+            }
+            set => _WarnsQuantity = value.GetValueOrDefault();
+        }
+        public string Welcome
+        {
+            get
+            {
+                if (_Welcome == null)
+                    return DefaultsHelper.DefaultWelcome();
+                return _Welcome;
+            }
+            set => _Welcome = value;
+        }
+        public string Rules
+        {
+            get
+            {
+                if (_Rules == null)
+                    return DefaultsHelper.DefaultRules();
+                return _Rules;
+            }
+            set => _Rules = value;
+        }
         public string DisabledCommands { get; set; }
     }
 }
