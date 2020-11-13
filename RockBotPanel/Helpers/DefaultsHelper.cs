55192a79b3c47535044ae3a2956ed8f9926b8006
Roker2 <dimaminko2000@gmail.com>
2020-11-13 19:52:51 +0300

    Helpers: DefaultsHelper: Init
    From my bot
    https://github.com/Roker2/RockBot/blob/master/modules/texts/defaults.go

---
+++ RockBotPanel/Helpers/DefaultsHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockBotPanel.Helpers
+{
+    public class DefaultsHelper
+    {
+        public static int DefaultWarnsQuantity() => 5;
+
+        public static string DefaultRules() => "Правила не установлены!";
+
+        public static string DefaultWelcome() => "Добро пожаловать, {firstName}!";
+    }
+}
