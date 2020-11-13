f3084885ac6313456f311ea9dd848a2bc3bf7a2f
Roker2 <dimaminko2000@gmail.com>
2020-11-13 14:12:43 +0300

    Migrations: M003 and M004
    It is experimental

---
+++ RockBotPanel/Migrations/20201113105420_M003.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RockBotPanel.Migrations
+{
+    public partial class M003 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChatIds",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateTable(
+                name: "Chatinfo",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WarnsQuantity = table.Column<int>(nullable: true),
+                    Welcome = table.Column<string>(nullable: true),
+                    Rules = table.Column<string>(nullable: true),
+                    DisabledCommands = table.Column<string>(nullable: true),
+                    TelegramUserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chatinfo", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Chatinfo_AspNetUsers_TelegramUserId",
+                        column: x => x.TelegramUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chatinfo_TelegramUserId",
+                table: "Chatinfo",
+                column: "TelegramUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Chatinfo");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ChatIds",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
---
+++ RockBotPanel/Migrations/20201113111208_M004.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RockBotPanel.Migrations
+{
+    public partial class M004 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Chatinfo");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ChatIds",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChatIds",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateTable(
+                name: "Chatinfo",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DisabledCommands = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rules = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TelegramUserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    WarnsQuantity = table.Column<int>(type: "int", nullable: true),
+                    Welcome = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chatinfo", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Chatinfo_AspNetUsers_TelegramUserId",
+                        column: x => x.TelegramUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chatinfo_TelegramUserId",
+                table: "Chatinfo",
+                column: "TelegramUserId");
+        }
+    }
+}
