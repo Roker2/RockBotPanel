afa3bf6584c3a1b26346318b475bc0f5dcb463a3
Roker2 <dimaminko2000@gmail.com>
2020-11-14 22:40:13 +0300

    Controllers: ChatinfoesController: Hello filters

--- RockBotPanel/Controllers/ChatinfoesController.cs
+++ RockBotPanel/Controllers/ChatinfoesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -11,6 +12,7 @@ using RockBotPanel.Models;
 
 namespace RockBotPanel.Controllers
 {
+    [Authorize]
     public class ChatinfoesController : Controller
     {
         private readonly UserManager<TelegramUser> userManager;
