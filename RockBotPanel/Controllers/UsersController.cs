9cbd1b7dfc8cfd273a2db028766d2b6b24c9509e
Roker2 <dimaminko2000@gmail.com>
2020-11-20 20:37:29 +0300

    Controllers: UsersController: Add Authorize filter

--- RockBotPanel/Controllers/UsersController.cs
+++ RockBotPanel/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -12,6 +13,7 @@ using RockBotPanel.Models;
 
 namespace RockBotPanel.Controllers
 {
+    [Authorize]
     public class UsersController : Controller
     {
         private readonly UserManager<TelegramUser> userManager;
