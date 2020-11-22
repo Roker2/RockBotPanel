0bf9485c594c348ddc660f2ae6283c4094814726
Roker2 <dimaminko2000@gmail.com>
2020-11-23 00:42:42 +0300

    Role: Add details page

--- RockBotPanel/Controllers/RoleController.cs
+++ RockBotPanel/Controllers/RoleController.cs
@@ -121,5 +121,38 @@ namespace RockBotPanel.Controllers
                 return View(model);
             }
         }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            // Find the role by Role ID
+            var role = await roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+
+            var model = new Details
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
+
+            // Retrieve all the Users
+            foreach (var user in userManager.Users)
+            {
+                // Need to set MultipleActiveResultSets=true to connection string
+                // If the user is in this role, add the username to
+                // Users property of EditRoleViewModel. This model
+                // object is then passed to the view for display
+                if (await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+
+            return View(model);
+        }
     }
 }
---
+++ RockBotPanel/ViewModels/Role/Details.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace RockBotPanel.ViewModels.Role
+{
+    public class Details
+    {
+        public string Id { get; set; }
+        public string RoleName { get; set; }
+        public List<string> Users { get; set; } = new List<string>();
+    }
+}
