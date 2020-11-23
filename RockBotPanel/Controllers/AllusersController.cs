6c28e3c00fa71f12a7e64de802a69ec618a1e39b
Roker2 <dimaminko2000@gmail.com>
2020-11-23 17:04:49 +0300

    Controllers: AllusersController: Add filter for admins

--- RockBotPanel/Controllers/AllusersController.cs
+++ RockBotPanel/Controllers/AllusersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using RockBotPanel.Models;
 
 namespace RockBotPanel.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class AllusersController : Controller
     {
         private readonly d940mhn2jd7mllContext _context;
