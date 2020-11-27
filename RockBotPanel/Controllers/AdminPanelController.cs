244eb2295575386863738449a30b62bcde123fcd
Roker2 <dimaminko2000@gmail.com>
2020-11-27 23:53:50 +0300

    Controllers: AdminPanelController: Add page "Send Mail"

--- RockBotPanel/Controllers/AdminPanelController.cs
+++ RockBotPanel/Controllers/AdminPanelController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RockBotPanel.Models;
+using RockBotPanel.Services;
+using RockBotPanel.ViewModels.AdminPanel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +12,53 @@ namespace RockBotPanel.Controllers
 {
     public class AdminPanelController : Controller
     {
+        private readonly UserManager<TelegramUser> userManager;
+        private readonly IEmailMessenger _emailMessenger;
+
+        public AdminPanelController(UserManager<TelegramUser> userManager,
+            IEmailMessenger emailMessenger)
+        {
+            this.userManager = userManager;
+            _emailMessenger = emailMessenger;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult SendMail()
+        {
+            EmailMessageViewModel model = new EmailMessageViewModel();
+            List<Microsoft.AspNetCore.Mvc.Rendering.SelectListItem> userList = new List<Microsoft.AspNetCore.Mvc.Rendering.SelectListItem>();
+            foreach (var user in userManager.Users)
+            {
+                userList.Add(new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem
+                {
+                    Text = $"{user.Email} ({user.UserName})",
+                    Value = user.Email,
+                    Selected = false
+                });
+            }
+            model.UserList = userList.OrderBy(u => u.Value).ToList();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult SendMail(EmailMessageViewModel model)
+        {
+            EmailMessage msg = new EmailMessage
+            {
+                From = model.From,
+                Subject = model.Subject,
+                Content = model.Content
+            };
+            foreach(var email in model.SelectedEmails)
+            {
+                msg.ToAddresses.Add(email);
+            }
+            _emailMessenger.SendMsg(msg);
+            return RedirectToAction("Index", "AdminPanel");
         }
     }
 }
---
+++ RockBotPanel/ViewModels/AdminPanel/EmailMessageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using RockBotPanel.Models;
+using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Collections.Generic;
+
+namespace RockBotPanel.ViewModels.AdminPanel
+{
+    public class EmailMessageViewModel
+    {
+        public string From { get; set; } = "Rock Bot Panel";
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public IEnumerable<string> SelectedEmails { get; set; }
+        public IEnumerable<Microsoft.AspNetCore.Mvc.Rendering.SelectListItem> UserList { get; set; }
+    }
+}
