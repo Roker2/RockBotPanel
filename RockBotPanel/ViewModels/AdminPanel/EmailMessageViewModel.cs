a95cfab62c26733ec1853512761821681c46a5b6
Roker2 <dimaminko2000@gmail.com>
2020-11-28 14:12:57 +0300

    ViewModels: AdminPanel: EmailMessageViewModel: Add [Required]

--- RockBotPanel/ViewModels/AdminPanel/EmailMessageViewModel.cs
+++ RockBotPanel/ViewModels/AdminPanel/EmailMessageViewModel.cs
@@ -5,13 +5,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RockBotPanel.ViewModels.AdminPanel
 {
     public class EmailMessageViewModel
     {
+        [Required]
         public string From { get; set; } = "Rock Bot Panel";
+        [Required]
         public string Subject { get; set; }
+        [Required]
         public string Content { get; set; }
         public IEnumerable<string> SelectedEmails { get; set; }
         public IEnumerable<Microsoft.AspNetCore.Mvc.Rendering.SelectListItem> UserList { get; set; }
