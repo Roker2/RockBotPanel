7b8510fac7829aa8c692a796420727c768f35a57
Roker2 <dimaminko2000@gmail.com>
2020-11-28 21:16:27 +0300

    Chating: Use alert from bootstrap for messages
    https://bootstrap-4.ru/docs/4.0/components/alerts/

--- RockBotPanel/wwwroot/js/chat.js
+++ RockBotPanel/wwwroot/js/chat.js
@@ -8,9 +8,18 @@ document.getElementById("sendButton").disabled = true;
 connection.on("ReceiveMessage", function (user, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     var encodedMsg = user + " says " + msg;
-    var li = document.createElement("li");
+    /*var li = document.createElement("li");
     li.textContent = encodedMsg;
-    document.getElementById("messagesList").appendChild(li);
+    document.getElementById("messagesList").appendChild(li);*/
+    var messageBox = document.createElement("div");
+    messageBox.classList.add("alert");
+    messageBox.classList.add("alert-dark");
+    var messageBoxHeader = document.createElement("h4");
+    messageBoxHeader.classList.add("alert-heading");
+    messageBoxHeader.innerHTML = user;
+    messageBox.appendChild(messageBoxHeader);
+    messageBox.innerHTML += msg;
+    document.getElementById("messagesList").appendChild(messageBox);
 });
 
 connection.start().then(function () {
