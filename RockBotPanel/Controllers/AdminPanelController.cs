fe40fa8fd8ac82c250b4d8427143556e52cea407
Roker2 <dimaminko2000@gmail.com>
2020-11-28 15:59:30 +0300

    AdminPanel: Add users list, deleting and editing users

--- RockBotPanel/Controllers/AdminPanelController.cs
+++ RockBotPanel/Controllers/AdminPanelController.cs
@@ -71,5 +71,116 @@ namespace RockBotPanel.Controllers
             _emailMessenger.SendMsg(msg);
             return RedirectToAction("Index", "AdminPanel");
         }
+
+        [HttpGet]
+        public IActionResult UsersList()
+        {
+            List<TelegramUser> users = userManager.Users.ToList();
+            return View(users);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                var result = await userManager.DeleteAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("UsersList");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View("UsersList");
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditUser(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                //_logger.LogError($"User with Id = {id} cannot be found");
+                ViewBag.ErrorMessage = $"User with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+
+            // GetClaimsAsync retunrs the list of user Claims
+            var userClaims = await userManager.GetClaimsAsync(user);
+            // GetRolesAsync returns the list of user Roles
+            var userRoles = await userManager.GetRolesAsync(user);
+            /*user.GenerateValidationCode();
+            var result = await userManager.UpdateAsync(user);
+
+            if (!result.Succeeded)
+            {
+                //_logger.LogError("Can not save validation code");
+                ViewBag.ErrorMessage = "Can not save validation code";
+                return View("NotFound");
+            }*/
+
+            var model = new EditUserViewModel
+            {
+                Id = user.Id,
+                Email = user.Email,
+                UserName = user.UserName,
+                TelegramId = user.TelegramId
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUser(EditUserViewModel model)
+        {
+            var user = await userManager.FindByIdAsync(model.Id);
+
+            if (user == null)
+            {
+                //_logger.LogError($"User with Id = {model.Id} cannot be found");
+                ViewBag.ErrorMessage = $"User with Id = {model.Id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                /*if (!user.CheckCode(model.Code))
+                {
+                    //_logger.LogError($"Bad code, {user.UserName} wrote: {model.Code}");
+                    ModelState.AddModelError(string.Empty, $"Bad code, you wrote: {model.Code}");
+                    return View(model);
+                }*/
+                user.TelegramId = model.TelegramId;
+                user.Email = model.Email;
+                user.UserName = model.UserName;
+
+                var result = await userManager.UpdateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("UsersList", "AdminPanel");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(model);
+            }
+        }
     }
 }
